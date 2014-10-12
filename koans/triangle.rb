e0412d715c580a84ed9b4a3eafa7c441fdfd5f34
pirvudoru <doru.pirvu@agilefreaks.com>
2014-10-12 22:38:28 +0300

    finishes about_triangle_project_2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  guard_negative_length(a, b, c)
+  guard_inconsistent_length(a, b, c)
+
   triangle_types = {
       3 => :scalene,
       2 => :isosceles,
@@ -22,6 +25,17 @@ def triangle(a, b, c)
   uniq_count = [a, b, c].uniq.size
 
   triangle_types[uniq_count]
+end
+
+def guard_negative_length(*args)
+  invalid_lengths = args.select { |l| l <= 0 }
+  raise TriangleError.new("Invalid length #{invalid_lengths}") if invalid_lengths.any?
+end
+
+def guard_inconsistent_length(*args)
+  raise TriangleError.new('Invalid lengths') if args.permutation.to_a.any? do |arr|
+    arr[0..-2].inject{ |sum, item| sum + item } <= arr.last
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
