109234446c983d904d8b086635a0695eb680d320
pirvudoru <doru.pirvu@agilefreaks.com>
2014-10-13 22:24:17 +0300

    finishes about_proxy_object_project

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -15,10 +15,30 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class Proxy
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def messages
+    @messages
+  end
+
+  def called?(method_name)
+    @messages.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    @messages.select { |message| message == method_name }.size
+  end
+
+  def method_missing(method_name, *args)
+    if @object.respond_to?(method_name)
+      @messages.push(method_name)
+
+      @object.__send__(method_name, *args)
+    else
+      super(method_name, *args)
+    end
+  end
 end
 
 # The proxy object should pass the following Koan:
